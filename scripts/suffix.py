0213cd873414ea37a16eb4c65fe1cc55b6b2e84a
marton <nagy@kyotorobotics.co.jp>
2020-03-14 17:21:28 +0900

    Suffix task done

---
+++ scripts/suffix.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: Marton
+
+import math
+import argparse
+
+suffixes = [
+    (0,   ''),
+    (3,   'Kilo'),
+    (6,   'Mega'),
+    (9,   'Giga'),
+    (12,  'Tera'),
+    (15,  'Peta'),
+    (18,  'Exa'),
+    (21,  'Zetta'),
+    (24,  'Yotta'),
+    (-3,  'Milli'),
+    (-6,  'Micro'),
+    (-9,  'Nano'),
+    (-12, 'Pico'),
+    (-15, 'Femto'),
+    (-18, 'Atto'),
+    (-21, 'Zepto'),
+    (-24, 'Yocto'),
+]
+# It is assumed that the suffixes are sorted by the suffix exponent, in reverse
+suffixes.sort(key=lambda x: x[0], reverse=True)
+
+
+def suffixWithUnit(number):
+    """
+    Returns the string representation of the given number, using suffixes such as Kilo, Mega, etc
+
+    suffixWithUnit(123) => 123
+    suffixWithUnit(1234) => 1.234 Kilo
+    suffixWithUnit(12345) => 12.345 Kilo
+    suffixWithUnit(1234567) => 1.234567 Mega
+    suffixWithUnit(12345678) => 12.345678 Mega
+
+    Args:
+        number (float): The number of which to return the string representation, using suffixes.
+
+    Returns:
+        str: the string representation of the given number, using suffixes
+    """
+    # 0 is a special case
+    if number == 0:
+        return "0"
+
+    # log10 of the given number's absolute value
+    try:
+        log_10 = math.log10(math.fabs(number))
+    except TypeError:
+        # math.log10 raises TypeError if the given number cannot be log10-ed
+        return "ERROR"
+
+    # Choose the most fitting suffix for the given number, based on the log10 of the number
+    suffix_exponent, suffix_name = next(((exp, name) for exp, name in suffixes
+                                         if exp <= log_10),
+                                        suffixes[-1])  # Default is the lowest possible suffix in the list
+
+    # Divide the number with 10^suffix_exponent
+    display_number = number/10**suffix_exponent
+
+    # Display text
+    display_text = "{:.16g} {}".format(display_number, suffix_name).strip()  # Python float is accurate to 16 digits
+
+    return display_text
+
+
+if __name__ == "__main__":
+    # Parse command line argument for the number
+    parser = argparse.ArgumentParser()
+    parser.add_argument("number", type=float,
+                        help="The number of which to return the string representation, using suffixes.")
+    args = parser.parse_args()
+
+    # Call the suffix function, print out result
+    print(suffixWithUnit(args.number))
---
+++ scripts/tests/test_suffix.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: Marton
+
+from ..suffix import *
+import pytest
+
+
+@pytest.mark.parametrize('number, expected', [
+    # Test all supported suffixes
+    (0,       "0"),
+    (10**0,   "1"),
+    (10**3,   "1 Kilo"),
+    (10**6,   "1 Mega"),
+    (10**9,   "1 Giga"),
+    (10**12,  "1 Tera"),
+    (10**15,  "1 Peta"),
+    (10**18,  "1 Exa"),
+    (10**21,  "1 Zetta"),
+    (10**24,  "1 Yotta"),
+    (10**-3,  '1 Milli'),
+    (10**-6,  '1 Micro'),
+    (10**-9,  '1 Nano'),
+    (10**-12, '1 Pico'),
+    (10**-15, '1 Femto'),
+    (10**-18, '1 Atto'),
+    (10**-21, '1 Zepto'),
+    (10**-24, '1 Yocto'),
+    # Test fractions
+    (1234.56789, "1.23456789 Kilo"),
+    # Test negative values
+    (-1234567, "-1.234567 Mega"),
+    # Tests from the assignment
+    (123,      "123"),
+    (1234,     "1.234 Kilo"),
+    (12345,    "12.345 Kilo"),
+    (1234567,  "1.234567 Mega"),
+    (12345678, "12.345678 Mega"),
+    # Numbers between 0 and 1
+    (0.1,        "100 Milli"),
+    (0.001,      "1 Milli"),
+    (0.00000123, "1.23 Micro"),
+    # Error handling
+    (None, "ERROR"),
+    ("1234", "ERROR")
+])
+def test_suffixWithUnit(number, expected):
+    assert suffixWithUnit(number) == expected
